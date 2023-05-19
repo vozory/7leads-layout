2c8c621716e3a10cb5b1ceb9fa0e89db8c72873f
vozory <vozory@gmail.com>
2023-05-19 06:47:22 +0300

    +

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,38 @@
+function toggleWindow() {
+    let el = document.getElementById('topMenu');
+    el.classList.toggle('main-menu--active');
+}
+
 function makeActive() {
     let activeElement = document.getElementById('active-list-item');
     activeElement.style.setProperty('grid-area', 'initial');
     activeElement.removeAttribute('id');
 
 
-    let element = document.getElementById("js_temp");
-    element.classList.add("is-active");
+    let element = document.getElementById('js_temp');
+    element.classList.add('is-active');
     
 }
+
+document.addEventListener('scroll', (event) => {
+    // lastKnownScrollPosition = window.scrollY;
+    // console.log(lastKnownScrollPosition);
+  
+    // if (lastKnownScrollPosition > 0) {
+    //     let topMenu = document.getElementById('topNav');
+    //     topMenu.classList.add('is-fixed');
+    // }
+
+
+    
+    // if (ticking) {
+        
+    //     window.requestAnimationFrame(() => {
+    //     doSomething(lastKnownScrollPosition);
+    //     ticking = false;
+    //   });
+  
+    //   ticking = true;
+    // }
+  });
+  
